2dc36cc87ca15e4faf436dbc1d4e484d45a23e46
Toxxxy <tonyxxxq@gmail.com>
2023-07-02 14:59:17 +0300

    fixed clearData

--- controllers/clearData.js
+++ controllers/clearData.js
@@ -3,23 +3,24 @@ const { Patient, Doctor, Appointment } = require('../models');
 exports.clearData = async (req, res) => {
     const deleteCounts = {};
     try {
+        const patients = await Patient.find();
+        const doctors = await Doctor.find();
+        const appointments = await Appointment.find();
+
         const deleteResult = await Promise.all([
             Patient.deleteMany(),
             Doctor.deleteMany(),
             Appointment.deleteMany(),
         ]);
 
-        deleteCounts.patients = deleteResult[0].deletedCount || 0;
-        deleteCounts.doctors = deleteResult[1].deletedCount || 0;
-        deleteCounts.appointments = deleteResult[2].deleteCount || 0;
+        deleteCounts.patients = deleteResult[0]?.deletedCount || 0;
+        deleteCounts.doctors = deleteResult[1]?.deletedCount || 0;
+        deleteCounts.appointments = deleteResult[2]?.deletedCount || 0;
 
         res.json({
-            patients: [],
-            doctors: [],
-            appointments: [],
-            successCount: 0,
-            formatErrors: [],
-            duplicateErrors: [],
+            patients,
+            doctors,
+            appointments,
             deleteCounts,
         });
     } catch (error) {
